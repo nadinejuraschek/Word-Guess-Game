17bc70feb29ce8e39e8521e327c555dd359a99b0
Nadine Ju <nadine.jura@online.de>
2019-10-05 15:19:19 -0700

    JS file: added wordContainer and defined function to let computer randomly choose a word

---
+++ assets/javascript/halloweenGame.js
@@ -0,0 +1,88 @@
+/****************************
+ SET UP: VARIABLES, FUNCTIONS 
+ ****************************/
+
+// Word Container
+var wordContainer = [
+    "bat",
+    "bewitching",
+    "bloody",
+    "bones",
+    "broom",
+    "candy",
+    "cemetery",
+    "coffin",
+    "costume",
+    "creaking",
+    "creepy",
+    "curse",
+    "devil",
+    "dracula",
+    "eerie",
+    "fear",
+    "foggy",
+    "frankenstein",
+    "frightened",
+    "ghost",
+    "goosebumps",
+    "grotesque",
+    "haunting",
+    "howl",
+    "lantern",
+    "magic",
+    "mask",
+    "monster",
+    "mummy",
+    "mysterious",
+    "nightmare",
+    "noise",
+    "petrified",
+    "phantom",
+    "potion",
+    "prank",
+    "pumpkin",
+    "quiver",
+    "scary",
+    "scream",
+    "shiver",
+    "shriek",
+    "skeleton",
+    "skull",
+    "sorcerer",
+    "spell",
+    "spider",
+    "spiderweb",
+    "spooky",
+    "supernatural",
+    "superstition",
+    "terror",
+    "tomb",
+    "torture",
+    "twilight",
+    "vampire",
+    "voodoo",
+    "weird",
+    "whipser",
+    "wicked",
+    "witch",
+    "witchcraft",
+    "wizard",
+    "wolf",
+    "zombie"
+];
+
+// Word Randomly Chosen by Computer
+var randomWord = "";
+var randomWordLetters = [];
+// Underscores Needed
+var underscores = 0;
+
+function chooseWord() {
+    // Let Computer Randomly Select a Word
+    randomWord = wordContainer[Math.floor(Math.random() * wordContainer.length)];
+    // split() "Slices" Word Into Individual Letters
+    randomWordLetters = randomWord.split("");
+    // How Many Underscores to Display
+    underscores = randomWord.length;
+}
+
